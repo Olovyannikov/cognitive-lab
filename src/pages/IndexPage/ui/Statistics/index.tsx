1b7c0b5a704a44e20e43e4726b11b64e8ced2e4a
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-03-02 22:11:57 +0300

    fix(safari): add image size for safari fix

--- src/pages/IndexPage/ui/Statistics/index.tsx
+++ src/pages/IndexPage/ui/Statistics/index.tsx
@@ -24,7 +24,7 @@ export const Statistics = () => {
                 <Card title='92%' text='пользователей согласны с&nbsp;точностью результатов' />
                 <Card title='8 из 10' text='нашли в отчёте ценные инсайты для дальнейшего развития' />
             </Flex>
-            <SpinningText reverse radius={7} className={s.spinning}>
+            <SpinningText radius={7} className={s.spinning}>
                 Cognitive Lab ✦ Cognitive Lab ✦
             </SpinningText>
         </Section>
