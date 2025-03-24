5efdcef296ee8c243a433d7d1867ad3eb5c6f080
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-03-24 19:11:26 +0300

    ci(build): update

---
+++ .vercel/output/static/assets/chunks/chunk-B7gipA3d.js
@@ -0,0 +1,1 @@
+import{J as a}from"./chunk-vBsGuFdY.js";const e=()=>a({name:"createPageStart",sid:"-jy57zs"});export{e as c};
--- .vercel/output/static/assets/chunks/chunk-CXiB2HZY.js
+++
@@ -1,1 +0,0 @@
-import{J as a}from"./chunk-71WK7kyK.js";const e=()=>a({name:"createPageStart",sid:"-jy57zs"});export{e as c};
