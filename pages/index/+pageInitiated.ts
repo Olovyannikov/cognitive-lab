0976b94d3fc6d3f10f85b37f517871f3cb4865cd
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-04-21 00:03:34 +0300

    fix(debugging): move model logic inside module

--- pages/index/+pageInitiated.ts
+++ pages/index/+pageInitiated.ts
@@ -5,7 +5,7 @@ import { createPageInit } from '@/shared/lib';
 import { getAllBlogPostsQuery } from '@/entities/Blog';
 import { getFAQQuery } from '@/entities/FAQ';
 import { getMainPageInfoQuery } from '@/entities/Landing';
-import { getReviewsQuery } from '@/entities/Review';
+import { getReviewsQuery, ReviewModel } from '@/entities/Review';
 
 export const pageInitiated = createPageInit();
 
@@ -28,4 +28,22 @@ sample({
         show_on_main: true,
     }),
     target: getFAQQuery.refresh,
+});
+
+sample({
+    clock: getReviewsQuery.finished.success,
+    fn: ({ result }) => ({
+        page_size: result.total_count,
+    }),
+    target: getReviewsQuery.refresh,
+});
+
+sample({
+    clock: getReviewsQuery.finished.success,
+    fn: ({ result }) =>
+        result.payload.map((data) => ({
+            ...data,
+            created_at: new Date(data.created_at).toLocaleDateString('ru-RU'),
+        })),
+    target: ReviewModel.$allReviews,
 });
