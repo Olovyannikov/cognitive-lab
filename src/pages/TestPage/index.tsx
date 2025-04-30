3a30c48cebe02a190a926b4f7bb47d4fd4bf7b09
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-04-30 22:07:13 +0300

    fix(test): test

--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -37,50 +37,50 @@ export const TestPage = () => {
     if (!data || !question) return null;
     if (isLoading) return <PageLoader />;
 
-    const Map: Record<string, ReactNode> = {
-        scale: (
-            <TestScaleQuestion
-                {...question}
-                text={phrases.texts[phraseIndex] ?? ''}
-                hint={phrases.hints[phraseIndex]}
-                value={String(value)}
-                page={page}
-                onChange={onChange}
-            />
-        ),
-        multiple_choice: question.options && (
-            <TestMultipleQuestion
-                {...question}
-                text={phrases.texts[phraseIndex] ?? ''}
-                hint={phrases.hints[phraseIndex]}
-                page={page}
-                onChange={onChange}
-                value={isArray(value) ? value : null}
-            />
-        ),
-        single_choice: question.options && (
-            <TestSingleChoiceQuestion
-                {...question}
-                text={phrases.texts[phraseIndex] ?? ''}
-                hint={phrases.hints[phraseIndex]}
-                page={page}
-                onChange={onChange}
-                showInput={Boolean(
-                    question.options.find((el) => el.id === (value as SingleChoiceAnswer)?.value)?.requires_input
-                )}
-                value={value as SingleChoiceAnswer}
-            />
-        ),
-    };
+    // const Map: Record<string, ReactNode> = {
+    //     scale: (
+    //         <TestScaleQuestion
+    //             {...question}
+    //             text={phrases.texts[phraseIndex] ?? ''}
+    //             hint={phrases.hints[phraseIndex]}
+    //             value={String(value)}
+    //             page={page}
+    //             onChange={onChange}
+    //         />
+    //     ),
+    //     multiple_choice: question.options && (
+    //         <TestMultipleQuestion
+    //             {...question}
+    //             text={phrases.texts[phraseIndex] ?? ''}
+    //             hint={phrases.hints[phraseIndex]}
+    //             page={page}
+    //             onChange={onChange}
+    //             value={isArray(value) ? value : null}
+    //         />
+    //     ),
+    //     single_choice: question.options && (
+    //         <TestSingleChoiceQuestion
+    //             {...question}
+    //             text={phrases.texts[phraseIndex] ?? ''}
+    //             hint={phrases.hints[phraseIndex]}
+    //             page={page}
+    //             onChange={onChange}
+    //             showInput={Boolean(
+    //                 question.options.find((el) => el.id === (value as SingleChoiceAnswer)?.value)?.requires_input
+    //             )}
+    //             value={value as SingleChoiceAnswer}
+    //         />
+    //     ),
+    // };
 
-    if (isSplashScreen) return <TestSplashScreen />;
+    // if (isSplashScreen) return <TestSplashScreen />;
 
     return (
         <TestContainer>
-            <TestProgress value={progress} page={page} total={data.length} />
-            {Map[question?.type]}
-            <Controls />
-            <SubmitTestModal />
+            {/*<TestProgress value={progress} page={page} total={data.length} />*/}
+            {/*{Map[question?.type]}*/}
+            {/*<Controls />*/}
+            {/*<SubmitTestModal />*/}
         </TestContainer>
     );
 };
