d41b093874c31be3c1da7f28bd541de343d429bf
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-04-30 22:24:16 +0300

    fix(test): test another route

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,4 +1,4 @@
-export const CDN_URL = import.meta.env.CDN_URL || 'https://cdn.cognitivelab.ru/assets';
+export const CDN_URL = 'https://cdn.cognitivelab.ru/assets';
 
 export const ROUTES = {
     TEST: '/mbti-test',
